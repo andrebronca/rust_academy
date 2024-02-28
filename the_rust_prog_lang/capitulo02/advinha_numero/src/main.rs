397c464103416e0c80ec3b09dc18cb89ed2e4ca9
André R. Bronca <bronca.andre@gmail.com>
2024-02-28 20:04:34 -0300

    Criando um programa que gera um número aleatório, pede para o usuário digitar um número e compara para ver se o usuário acertou. Caso seja digitado um valor inválido, o programa não quebra, continua executando. Caso o usuário acerte o número o programa finaliza.

--- the_rust_prog_lang/capitulo02/advinha_numero/src/main.rs
+++ the_rust_prog_lang/capitulo02/advinha_numero/src/main.rs
@@ -1,14 +1,44 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
-    println!("Advinhe o número!");
+    println!("Será sorteado um número de 1 a 100.");
 
-    println!("Digite um número.");
-    let mut guess = String::new();
+    let secret_number = rand::thread_rng()
+        .gen_range(1..=100);
+    
+    loop{
+        //após finalizado, não deve exibir o secret_number
+        //println!("O número secreto é: {secret_number}");
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Falha para ler a linha!");
+        println!("Tente advinhar, digite um número.");
+        //criando uma variável mutável
+        let mut guess = String::new();
 
-    println!("Você acertou: {guess}");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Falha para ler a linha!");
+
+        //convertendo string para número
+        let guess: u32 = match guess.trim()
+            .parse() {
+                Ok(num) => num,
+                Err(_) => continue,
+            };
+            //.expect("Insira um número para funcionar!");
+
+        //println!("Você acertou: {guess}");
+        //outra forma
+        println!("digitou {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Muito baixo!"),
+            Ordering::Greater => println!("Muito alto!"),
+            Ordering::Equal => {
+                println!("Você acertou!!!");
+                break;
+            }
+        }
+    }
 }
